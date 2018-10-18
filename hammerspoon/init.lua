e2a1a3c05654de6ba6ac77bf23ec25396750050b
zwPapEr <zw.paper@gmail.com>
2018-10-18 16:32:51 +0800

    karabiner: :hammer: keyboard changing by karabiner rather than hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -13,41 +13,41 @@ configFileWatcher:start()
 hs.window.animationDuration = 0
 
 
--- Keyboard Settting
----- karabiner-elements
-keyboardWatcher = nil
-deleteKarabinerCMD = "tell application \"Finder\" to delete \"/Users/didi/.config/karabiner/karabiner.json\" as POSIX file"
-copyKarabinerHHKBcmd = "do shell script \"cp /Users/didi/.config/karabiner/karabiner.json.hhkb /Users/didi/.config/karabiner/karabiner.json\""
-copyKarabinerPokerCMD = "do shell script \"cp /Users/didi/.config/karabiner/karabiner.json.poker /Users/didi/.config/karabiner/karabiner.json\""
-copyKarabinerMacCMD = "do shell script \"cp /Users/didi/.config/karabiner/karabiner.json.mac /Users/didi/.config/karabiner/karabiner.json\""
-function keyboardCallback(data)
-    if (data["eventType"] == "added") then
-        if (data["productID"] == 256) then
-            hs.notify.show("hhkb inserted", "", "")
-            local deleteResult = hs.osascript.applescript(deleteKarabinerCMD)
-            local copyResult = hs.osascript.applescript(copyKarabinerHHKBcmd)
-            hs.notify.show("Load HHKB karabiner config",
-                           "delete old "..(deleteResult and "succ" or "fail"),
-                           "load "..(copyResult and "succ" or "fail"))
-        elseif (data["productID"] == 1553) then
-            hs.notify.show("poker inserted", "", "")
-            local deleteResult = hs.osascript.applescript(deleteKarabinerCMD)
-            local copyResult = hs.osascript.applescript(copyKarabinerPokerCMD)
-            hs.notify.show("Load Poker karabiner config",
-                           "delete old "..(deleteResult and "succ" or "fail"),
-                           "load "..(copyResult and "succ" or "fail"))
-        end
-    elseif (data["productID"] == 256) or (data["productID"] == 1553) then
-            hs.notify.show("keyboar unplugged", "", "")
-            local deleteResult = hs.osascript.applescript(deleteKarabinerCMD)
-            local copyResult = hs.osascript.applescript(copyKarabinerMacCMD)
-            hs.notify.show("Load HHKB karabiner config",
-                           "delete old "..(deleteResult and "succ" or "fail"),
-                           "load "..(copyResult and "succ" or "fail"))
-    end
-end
-keyboardWatcher = hs.usb.watcher.new(keyboardCallback)
-keyboardWatcher:start()
+-- -- Keyboard Settting
+-- ---- karabiner-elements
+-- keyboardWatcher = nil
+-- deleteKarabinerCMD = "tell application \"Finder\" to delete \"/Users/didi/.config/karabiner/karabiner.json\" as POSIX file"
+-- copyKarabinerHHKBcmd = "do shell script \"cp /Users/didi/.config/karabiner/karabiner.json.hhkb /Users/didi/.config/karabiner/karabiner.json\""
+-- copyKarabinerPokerCMD = "do shell script \"cp /Users/didi/.config/karabiner/karabiner.json.poker /Users/didi/.config/karabiner/karabiner.json\""
+-- copyKarabinerMacCMD = "do shell script \"cp /Users/didi/.config/karabiner/karabiner.json.mac /Users/didi/.config/karabiner/karabiner.json\""
+-- function keyboardCallback(data)
+--     if (data["eventType"] == "added") then
+--         if (data["productID"] == 256) then
+--             hs.notify.show("hhkb inserted", "", "")
+--             local deleteResult = hs.osascript.applescript(deleteKarabinerCMD)
+--             local copyResult = hs.osascript.applescript(copyKarabinerHHKBcmd)
+--             hs.notify.show("Load HHKB karabiner config",
+--                            "delete old "..(deleteResult and "succ" or "fail"),
+--                            "load "..(copyResult and "succ" or "fail"))
+--         elseif (data["productID"] == 1553) then
+--             hs.notify.show("poker inserted", "", "")
+--             local deleteResult = hs.osascript.applescript(deleteKarabinerCMD)
+--             local copyResult = hs.osascript.applescript(copyKarabinerPokerCMD)
+--             hs.notify.show("Load Poker karabiner config",
+--                            "delete old "..(deleteResult and "succ" or "fail"),
+--                            "load "..(copyResult and "succ" or "fail"))
+--         end
+--     elseif (data["productID"] == 256) or (data["productID"] == 1553) then
+--             hs.notify.show("keyboar unplugged", "", "")
+--             local deleteResult = hs.osascript.applescript(deleteKarabinerCMD)
+--             local copyResult = hs.osascript.applescript(copyKarabinerMacCMD)
+--             hs.notify.show("Load HHKB karabiner config",
+--                            "delete old "..(deleteResult and "succ" or "fail"),
+--                            "load "..(copyResult and "succ" or "fail"))
+--     end
+-- end
+-- keyboardWatcher = hs.usb.watcher.new(keyboardCallback)
+-- keyboardWatcher:start()
 
 -- right option to chinese
 noInput = "com.apple.keylayout.US"
