fea911e42270b70a06bb6921fd8c4535bda13c3a
Zhang Wei <zhangwei@Wei.local>
2018-11-02 14:10:20 +0800

    sync: :sparkles: sync spelling

--- init/macos.sh
+++ init/macos.sh
@@ -26,8 +26,6 @@ karabiner-elements
 hammerspoon
 istat-menus
 quitter
-bettertouchtool
-unlox
 dropbox
 google-chrome
 1password
@@ -40,6 +38,13 @@ slack
 wechat
 neteasemusic
 "
+## Homebrew Apps
+brewApps="
+zsh
+emacs
+golang
+global
+"
 
 # Mac tools
 which brew
@@ -48,9 +53,37 @@ if [ X$result == X1 ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+export HOMEBREW_NO_AUTO_UPDATE=1
+for i in $brewApps; do
+    brew install $i
+done
 for i in $caskApps; do
     brew cask install $i
 done
+
+# dev software
+# dev env
+if [ ! -d $repoPath ]; then
+    # this init.sh is saved in repo, should not go here
+    echo "no repo path found"
+    exit 1
+fi
+## oh my zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo "setting zsh..."
+    brew install zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    rm -f ~/.zshrc
+    ln -s ~/repo/dotfile/zshrc ~/.zshrc
+    echo "zsh done..."
+fi
+## Emacs
+if [ ! -h ~/.emacs.d ]; then
+    echo "setting emacs..."
+    mv ~/.emacs.d ~/emacs.d.bak
+    ln -s $dotPath/emacs.d ~/.emacs.d
+    echo "emacs done"
+fi
 
 # karabiner-elements
 if [ ! -f ~/.config/karabiner/karabiner.json ]; then
@@ -60,23 +93,11 @@ if [ ! -f ~/.config/karabiner/karabiner.json ]; then
     echo "karabiner done"
 fi
 
-
-# dev software
-# brew install emacs
-
-# dev env
-if [ ! -d $repoPath ]; then
-    mkdir -p $repoPath
-fi
-## clone dotfile
-if [ ! -d $dotPath ]; then
-    cd $repoPath
-    git clone $dotRepoGit
-fi
-## oh my zsh
-if [ ! -d ~/.oh-my-zsh ]; then
-    brew install zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    rm -f ~/.zshrc
-    ln -s ~/repo/dotfile/zshrc ~/.zshrc
+# Sync
+## Spelling
+if [ ! -h ~/Library/Spelling ]; then
+    echo "sync spelling..."
+    sudo mv ~/Library/Spelling/ ~/Library/Spelling.bak
+    ln -s ~/Dropbox/AppSync/Spelling ~/Library/Spelling
+    echo "spelling done"
 fi
