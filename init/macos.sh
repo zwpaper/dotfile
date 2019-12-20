f8844f5ad3d042fe8301dcf4738ecfacb2d6d835
zwPapEr <zw.paper@gmail.com>
2019-12-20 14:24:04 +0800

    init/mac: :hammer: use mac for mac only

--- init/macos.sh
+++ init/macos.sh
@@ -4,25 +4,18 @@ echo "* touchpad disable three key functions"
 echo "* add ssh key to github"
 
 # Env
-repoPath=$HOME"/repo"
+repoPath=$HOME"/code/repo"
 dotRepo="dotfile"
-dotPath=$repoPath"/"$dotRepo
+dotPath=$repoPath/$dotRepo
 dotRepoGit="git@github.com:zwpaper/dotfile.git"
+dotRepoHTTPS="https://github.com/zwpaper/dotfile.git"
 
 ## Application list
 ## Mac App Store
 masApps="
-bear
 reeder
 spark
-mweb
 xnip
-airmail
-"
-## Homebrew Cask
-brewApps="
-global
-golang
 "
 ## Homebrew Cask
 caskApps="
@@ -35,7 +28,6 @@ quitter
 dropbox
 google-chrome
 1password
-qingg
 iterm2
 visual-studio-code
 postman
@@ -48,9 +40,6 @@ font-source-code-pro
 ## Homebrew Apps
 brewApps="
 zsh
-emacs
-golang
-global
 bat
 lsd
 "
@@ -60,46 +49,44 @@ gnu-sed
 "
 ### end macOS software
 
-### start linux software
-linuxCliApps="
-emacs-nox
-base-devel
-"
-
 ### Start installing
 os=`uname`
 case $os in
     Darwin)
-	echo "Installing apps in macOS"
-	# Mac tools
-	which brew
-	result=`echo $?`
-	if [ X$result == X1 ]; then
-	    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	    brew tap caskroom/fonts
-	fi
+        echo "Installing apps in macOS"
+        # Mac tools
+        which brew
+        result=`echo $?`
+        if [ X$result == X1 ]; then
+            /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            brew tap homebrew/cask-fonts
+        fi
 
-	export HOMEBREW_NO_AUTO_UPDATE=1
-	for i in $brewApps; do
-	    brew install $i
-	done
-	for i in $gnuApps; do
-	    brew install $i --with-default-names
-	done
-	for i in $caskApps; do
-	    brew cask install $i
-	done
+        export HOMEBREW_NO_AUTO_UPDATE=1
+        for i in $brewApps; do
+            echo "Installing "$i
+            brew install $i
+        done
+        for i in $gnuApps; do
+            echo "Installing "$i
+            brew install $i --with-default-names
+        done
+        for i in $caskApps; do
+            echo "Installing "$i
+            brew cask install $i
+        done
+        # alfred 3
+        curl -SLo /tmp/alfred3.dmg https://cachefly.alfredapp.com/Alfred_3.8.6_972.dmg
+        hdiutil mount /tmp/alfred3.dmg
+        sudo cp -R /Volumes/Alfred/Alfred\ 3.app /Applications
+        hdiutil unmount /Volumes/Alfred
         ;;
     Linux)
-	echo "Installing apps in Linux"
-	# Default using arch linux, may add other later
-	pacman -S $linuxCliApps --noconfirm
+        echo "Use Linux.sh"
         ;;
 esac
 
 ### End installing
-
-brew tap homebrew/cask-fonts && brew cask install font-source-code-pro
 
 # dev software
 # dev env
@@ -111,35 +98,25 @@ fi
 ## oh my zsh
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "setting zsh..."
-    brew install zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     rm -f ~/.zshrc
-    ln -s ~/repo/dotfile/zshrc ~/.zshrc
+    ln -s $dotPath/zshrc ~/.zshrc
     echo "zsh done..."
 fi
-## Emacs
-if [ ! -h ~/.emacs.d ]; then
-    echo "setting emacs..."
-    mv ~/.emacs.d ~/emacs.d.bak
-    ln -s $dotPath/emacs.d ~/.emacs.d
-    echo "emacs done"
+
+# karabiner-elements
+if [ ! -f ~/.config/karabiner/karabiner.json ]; then
+    echo "setting karabiner..."
+    mv ~/.config/karabiner ~/.config/karabiner.bak
+    ln -s $dotPath/karabiner ~/.config/karabiner
+    echo "karabiner done"
 fi
 
-if [ $os == "Darwin" ] ; then
-    # karabiner-elements
-    if [ ! -f ~/.config/karabiner/karabiner.json ]; then
-	echo "setting karabiner..."
-	mv ~/.config/karabiner ~/.config/karabiner.bak
-	ln -s $dotPath/karabiner ~/.config/karabiner
-	echo "karabiner done"
-    fi
-
-    # Sync
-    ## Spelling
-    if [ ! -h ~/Library/Spelling ]; then
-	echo "sync spelling..."
-	sudo mv ~/Library/Spelling/ ~/Library/Spelling.bak
-	ln -s ~/Dropbox/AppSync/Spelling ~/Library/Spelling
-	echo "spelling done"
-    fi
+# Sync
+## Spelling
+if [ ! -h ~/Library/Spelling ]; then
+    echo "sync spelling..."
+    sudo mv ~/Library/Spelling/ ~/Library/Spelling.bak
+    ln -s ~/Dropbox/AppSync/Spelling ~/Library/Spelling
+    echo "spelling done"
 fi
