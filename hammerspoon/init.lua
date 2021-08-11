d8fc33c97db46693eee22ec8fe47a326a0f11eec
zwPapEr <kweizh@gmail.com>
2021-08-11 15:49:05 +0800

    keyboard: :sparkles: add model window ops
    Signed-off-by: zwPapEr <kweizh@gmail.com>

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -10,6 +10,7 @@ configFileWatcher:start()
 
 -- Misc setup
 hs.window.animationDuration = 0
+hs.alert.defaultStyle['atScreenEdge'] = 2
 
 --- input source
 ---
@@ -62,4 +63,61 @@ hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "9",
          hs.alert.show("Can not change input source to us")
       end
       hs.eventtap.keyStroke({"cmd", "alt", "ctrl", "shift"}, "7")
+end)
+
+--- Model Window Ops
+--Predicate that checks if a window belongs to a screen
+function isInScreen(screen, win)
+   return win:screen() == screen
+end
+
+function focusScreen(screen)
+   --Get windows within screen, ordered from front to back.
+   --If no windows exist, bring focus to desktop. Otherwise, set focus on
+   --front-most application window.
+   local windows = hs.fnutils.filter(
+      hs.window.orderedWindows(),
+      hs.fnutils.partial(isInScreen, screen))
+   local windowToFocus = #windows > 0 and windows[1] or hs.window.desktop()
+   windowToFocus:focus()
+
+   -- Move mouse to center of screen
+   local pt = hs.geometry.rectMidPoint(screen:fullFrame())
+   hs.mouse.setAbsolutePosition(pt)
+end
+
+k = hs.hotkey.modal.new('cmd-ctrl', 'm')
+function k:entered() hs.alert('Entered window mode') end
+function k:exited()  hs.alert('Exited window mode')  end
+k:bind('', 'escape', function() k:exit() end)
+k:bind('', 'o', 'Other screen', function()
+          hs.window.focusedWindow():moveToScreen(hs.screen.mainScreen():next(),ensureInScreenBounds)
+end)
+k:bind('', 'e', 'East Screen', function()
+          hs.window.focusedWindow():moveOneScreenEast(ensureInScreenBounds, 0)
+end)
+k:bind('', 'w', 'West Screen', function()
+          hs.window.focusedWindow():moveOneScreenWest(ensureInScreenBounds, 0)
+end)
+k:bind('', 'm', 'Maximize', function()
+          hs.window.focusedWindow():maximize(0)
+end)
+k:bind('', 'f', 'Focus Other Screen', function()
+          focusScreen(hs.window.focusedWindow():screen():next())
+end)
+k:bind('', 'k', 'Kill Focused Screen', function()
+          hs.window.focusedWindow():close()
+end)
+k:bind('', 'n', 'Next Space', function()
+          hs.eventtap.keyStroke({"ctrl"}, 'right')
+end)
+k:bind('', 'p', 'Previous Space', function()
+          hs.eventtap.keyStroke({"ctrl", "fn"}, ';')
+end)
+k:bind('', 'a', 'Previous Space', nil, function()
+          hs.eventtap.keyStroke({"ctrl"}, 'right')
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "5", nil, function()
+      hs.eventtap.keyStroke({"cmd", "alt", "ctrl", "shift"}, "2")
 end)
